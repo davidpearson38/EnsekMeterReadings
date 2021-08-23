1e9def00795a40c02efbbfe63d0c0bead4720760
davidpearson38 <davidpearson38@gmail.com>
2021-08-23 23:30:26 +0100

    DB classes and context

---
+++ Projects/EnsekMeterReadings.Business/Data/Entities/DbAccount.cs
@@ -0,0 +1,18 @@
+﻿namespace EnsekMeterReadings.Business.Data.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("Account")]
+    public class DbAccount
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+    }
+}
---
+++ Projects/EnsekMeterReadings.Business/Data/MeterReadingsContext.cs
@@ -0,0 +1,18 @@
+﻿namespace EnsekMeterReadings.Business.Data
+{
+    using EnsekMeterReadings.Business.Data.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class MeterReadingsContext : DbContext
+    {
+        public DbSet<DbAccount> Accounts { get; set; }
+
+        public DbSet<DbMeterReading> MeterReadings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // TODO: Put this in env variable or in startup
+            optionsBuilder.UseSqlServer(@"Server=(local);Database=EnsekMeterReadings;Trusted_Connection=True;");
+        }
+    }
+}
