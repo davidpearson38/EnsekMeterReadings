cf93f7c934c2e49f2e959197bff836a04bccbb71
davidpearson38 <davidpearson38@gmail.com>
2021-08-23 23:31:50 +0100

    Add class for parsing individual CSV rows

---
+++ Projects/EnsekMeterReadings.Business/CsvParsing/MeterReadingCsvRowParseResult.cs
@@ -0,0 +1,31 @@
+﻿using EnsekMeterReadings.Domain;
+
+namespace EnsekMeterReadings.Business.CsvParsing
+{
+    public class MeterReadingCsvRowParseResult
+    {
+        private MeterReadingCsvRowParseResult(
+            string error,
+            MeterReading meterReading)
+        {
+            this.Error = error;
+            this.MeterReading = meterReading;
+        }
+
+        public string Error { get; }
+
+        public bool IsSuccess => string.IsNullOrEmpty(this.Error);
+
+        public MeterReading MeterReading { get; }
+
+        public static MeterReadingCsvRowParseResult Success(MeterReading meterReading)
+        {
+            return new MeterReadingCsvRowParseResult(null, meterReading);
+        }
+
+        public static MeterReadingCsvRowParseResult Failure(string error)
+        {
+            return new MeterReadingCsvRowParseResult(error, null);
+        }
+    }
+}
---
+++ Projects/EnsekMeterReadings.Business/CsvParsing/MeterReadingCsvRowParser.cs
@@ -0,0 +1,65 @@
+﻿namespace EnsekMeterReadings.Business.CsvParsing
+{
+    using EnsekMeterReadings.Domain;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class MeterReadingCsvRowParser
+    {
+        public static MeterReadingCsvRowParseResult Parse(
+            IReadOnlyCollection<Account> accounts,
+            string meterReadingCsvRow)
+        {
+            var csvValues = meterReadingCsvRow.Split(",");
+
+            if (csvValues.Length < 3)
+            {
+                return MeterReadingCsvRowParseResult.Failure(
+                    $"{meterReadingCsvRow} contains an insufficient number of values.");
+            }
+            
+            var accountIdString = csvValues[0].Trim();
+
+            if (!int.TryParse(accountIdString, out var accountId))
+            {
+                return MeterReadingCsvRowParseResult.Failure(
+                    $"{accountIdString} is not a valid Account ID.");
+            }
+
+            var account = accounts.SingleOrDefault(acc => acc.Id == accountId);
+
+            if (account == null)
+            {
+                return MeterReadingCsvRowParseResult.Failure(
+                    $"{accountId} is not a recognised Account ID.");
+            }
+
+            var meterReadingDateTimeString = csvValues[1].Trim();
+
+            if (!DateTime.TryParse(meterReadingDateTimeString, out var meterReadingDateTime))
+            {
+                return MeterReadingCsvRowParseResult.Failure(
+                    $"{meterReadingDateTimeString} is not a valid date/time.");
+            }
+
+            var valueString = csvValues[2].Trim();
+
+            if (!int.TryParse(valueString, out var value))
+            {
+                return MeterReadingCsvRowParseResult.Failure(
+                    $"{valueString} is not a valid meter reading value.");
+            }
+
+            if (value < 0 || value > 99999)
+            {
+                return MeterReadingCsvRowParseResult.Failure(
+                    $"{value} is not within the valid meter reading value range of 0-99999.");
+            }
+
+            var meterReading = new MeterReading(account, meterReadingDateTime, value);
+
+            return MeterReadingCsvRowParseResult.Success(meterReading);
+        }
+    }
+}
