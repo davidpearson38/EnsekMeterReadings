35f1651add0f152ae88ad4a3610ee9a7195d9c13
davidpearson38 <davidpearson38@gmail.com>
2021-08-23 23:29:37 +0100

    Domain to represent core classes

---
+++ Projects/EnsekMeterReadings.Domain/Account.cs
@@ -0,0 +1,18 @@
+﻿namespace EnsekMeterReadings.Domain
+{
+    public class Account
+    {
+        public Account(int id, string firstName, string lastName)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public int Id { get; }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+    }
+}
---
+++ Projects/EnsekMeterReadings.Domain/MeterReading.cs
@@ -0,0 +1,20 @@
+﻿namespace EnsekMeterReadings.Domain
+{
+    using System;
+
+    public class MeterReading
+    {
+        public MeterReading(Account account, DateTime meterReadingDateTime, int value)
+        {
+            this.Account = account;
+            this.MeterReadingDateTime = meterReadingDateTime;
+            this.Value = value;
+        }
+
+        public Account Account { get; }
+
+        public DateTime MeterReadingDateTime { get; }
+
+        public int Value { get; }
+    }
+}
